e20adefd532db3b3caa939a04a8178bc9e04eaee
dwjohnston93 <dwjohnston93@gmail.com>
2020-03-15 22:18:42 -0700

    Form Validation and Final Styling -add getHighest function which sets the input's max value at the latest comic's number -prev and next buttons will not attempt a http request if the comic is at 1 or latest comic number -updated styling

--- public/request.js
+++ public/request.js
@@ -1,3 +1,4 @@
+//function updates HTML with latest comic received from server, and current comic number
 function updateHTML(comic){
     let title = document.getElementById('comic-title');
     let img = document.getElementById('comic-img');
@@ -5,10 +6,12 @@ function updateHTML(comic){
         title.innerHTML = `#${comic.num} ${comic.title}`;
         img.src = comic.img;
         img.alt = comic.alt;
+        comicNum = comic.num
     } else{
         title.innerHTML = `#${comic.num} ${comic.title}`;
         img.src = comic.img;
         img.alt = comic.alt;
+        comicNum = comic.num
         //add prev and next buttons to page after intial random call
         let prev = document.createElement('button');
         let next = document.createElement('button');
@@ -17,6 +20,8 @@ function updateHTML(comic){
         let buttons = document.getElementById('buttons');
         prev.innerText = "<Prev";
         next.innerText = "Next>";
+        prev.className = "button"
+        next.className = "button"
         buttons.prepend(prev);
         buttons.appendChild(next);
     }
@@ -25,11 +30,24 @@ function updateHTML(comic){
 
 let baseURL = "http://localhost:8080"
 
-//GET KEYBOARD ENTER FOR THIS TOO
+let comicNum;
+let latestComic;
+
+//gets latest comic number for form validation controls
+const getHighest = () => {
+    const promise = fetch(`${baseURL}/highest`);
+    promise.then(res => {
+        return res.json();
+    }).then(max => {
+        latestComic = max.num;
+        let input = document.getElementById('search');
+        input.max = max.num;
+    })
+}
+
 const getNum = () => {
     let number = document.getElementById("search").value;
     let numObj = {num : number};
-    console.log("numObj:", numObj)
     const promise = fetch(`${baseURL}/num`, {
         method : 'POST',
         headers: { 
@@ -41,7 +59,6 @@ const getNum = () => {
     promise.then(res => {
         return res.json();
     }).then(comic => {
-        console.log("comic:", comic)
         updateHTML(comic)
     })
 }
@@ -51,12 +68,15 @@ const getRandom = () => {
     promise.then(res => {
         return res.json();
     }).then(comic => {
-        console.log("comic:", comic)
         updateHTML(comic)
     })
 }
 
 const getPrev = () => {
+    if(comicNum === 1){
+        alert("You are at the first comic");
+        return;
+    }
     const promise = fetch(`${baseURL}/prev`);
     promise.then(res => {
         return res.json();
@@ -66,6 +86,10 @@ const getPrev = () => {
 }
 
 const getNext = () => {
+    if(comicNum === latestComic){
+        alert("You are at the latest comic");
+        return;
+    }
     const promise = fetch(`${baseURL}/next`);
     promise.then(res => {
         return res.json();
--- server.js
+++ server.js
@@ -18,32 +18,36 @@ let latestNum;
 let currentNum;
 
 app.get('/', (req, res) => {
-    latestComic()
+    latestComic();
     res.sendFile('public/index.html', { root: __dirname });
 });
+
+app.get('/highest', (req, res) => {
+    res.json({num : latestNum})
+})
 
 
 app.post('/num', (req, res) => {
     currentNum = req.body.num;
-    const prevComic = fetch(`http://xkcd.com/${currentNum}/info.0.json`);
-    prevComic.then(response => {
-     return response.json();
+    const numComic = fetch(`http://xkcd.com/${currentNum}/info.0.json`);
+    numComic.then(response => {
+        return response.json();
     }).then(data => {
-        res.json(data)
+        res.json(data);
     }).catch(error => {
-    console.log("e:", error);
+        console.log("e:", error);
     });
 });
 
 app.get('/random', (req, res) => {
-    currentNum = getRandomInt(1, latestNum)
+    currentNum = getRandomInt(1, latestNum);
     const randomComic = fetch(`http://xkcd.com/${currentNum}/info.0.json`);
     randomComic.then(response => {
-     return response.json();
+        return response.json();
     }).then(data => {
-        res.json(data)
+        res.json(data);
     }).catch(error => {
-    console.log("e:", error);
+        console.log("e:", error);
     });
 });
 
@@ -51,29 +55,29 @@ app.get('/prev', (req, res) => {
     currentNum--;
     const prevComic = fetch(`http://xkcd.com/${currentNum}/info.0.json`);
     prevComic.then(response => {
-     return response.json();
+        return response.json();
     }).then(data => {
-        res.json(data)
+        res.json(data);
     }).catch(error => {
-    console.log("e:", error);
+        console.log("e:", error);
     });
 });
 
 app.get('/next', (req, res) => {
     currentNum++;
-    const prevComic = fetch(`http://xkcd.com/${currentNum}/info.0.json`);
-    prevComic.then(response => {
-     return response.json();
+    const nextComic = fetch(`http://xkcd.com/${currentNum}/info.0.json`);
+    nextComic.then(response => {
+        return response.json();
     }).then(data => {
-        res.json(data)
+        res.json(data);
     }).catch(error => {
-    console.log("e:", error);
+        console.log("e:", error);
     });
 });
 
 app.get('*', function(req, res){
     res.sendFile('public/404.html', { root: __dirname });
-  });
+});
 
 //updates the latestNum for use in the random call
 const latestComic = fetch('http://xkcd.com/info.0.json');
@@ -81,11 +85,10 @@ latestComic.then(response => {
   return response.json()
   .then(data => {
     latestNum = data.num;
-    return;
   })
 }).catch(error => {
     console.log("e:", error);
-  });
+});
 
 //generates random number between 0 and latest comic issue number
 function getRandomInt(min, max) {
